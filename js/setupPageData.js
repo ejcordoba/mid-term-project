500985b638d0b62dc17f4cd5d0083aab28c379b4
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-11-29 18:25:51 +0100

    changing main root file for netlify deploy

--- js/setupPageData.js
+++ js/setupPageData.js
@@ -1,25 +1,25 @@
-if (localStorage.getItem('id') !== null) {
-    const beerId = localStorage.getItem('id');
-    const getBeerDetail = async () => {
-        const getBeerDetailFetchResponse = await fetch(`https://api.punkapi.com/v2/beers/${beerId}`)
-        const beerData = await getBeerDetailFetchResponse.json()
-        return beerData
-    }
+const beerId = localStorage.getItem('id');
+const getBeerDetail = async () => {
+    const getBeerDetailFetchResponse = await fetch(`https://api.punkapi.com/v2/beers/${beerId}`)
+    const beerData = await getBeerDetailFetchResponse.json()
+    return beerData
 }
-const main = async() => {
+const setupPage = async() => {
     const beerData = await getBeerDetail()
+    console.log(beerData);
     updateBeerData(beerData);
 }
 function updateBeerData(beerData) {
     console.log(beerData);
-    /* const projectCards = document.querySelectorAll('.project-card');
-    projectCards.forEach(projectCard => {
-        const randomBeer = allBeersData[Math.floor(Math.random() * allBeersData.length)];
-        projectCard.querySelector('.project-card-image').src = randomBeer.image_url;
-        projectCard.querySelector('.project-title').innerHTML = randomBeer.name;
-        projectCard.querySelector('.project-description').innerHTML = randomBeer.description;
-        projectCard.querySelector('.project-link').id = randomBeer.id;
-        projectCard.addEventListener('click', setProjectDetail);
-    }) */
+    document.querySelector('.project-page-title').innerHTML = beerData[0].name;
+    document.querySelector('.project-page-type').innerHTML = beerData[0].tagline;
+    document.querySelector('.project-completed').innerHTML = 'First brewed: ';
+    document.querySelector('.project-date').innerHTML = beerData[0].first_brewed;
+    document.querySelector('.project-page-image').src = beerData[0].image_url;
+    document.querySelector('.project-description').innerHTML = beerData[0].description;
+    document.querySelector('.project-brewers-tips').innerHTML = beerData[0].brewers_tips;
+    // projectCard.querySelector('.project-description').innerHTML = randomBeer.description;
+    // projectCard.querySelector('.project-link').id = randomBeer.id;
+   
 }
-main()
+setupPage()
